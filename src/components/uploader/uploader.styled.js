ae5624f76bc03f9ac14d20dc38898067013ebbe7
Omar Houmz <omhoumz@gmail.com>
2020-02-29 12:51:58 +0100

    chore: prettier the file uploader.styled.js

--- src/components/uploader/uploader.styled.js
+++ src/components/uploader/uploader.styled.js
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import { colors, radius, spacing } from "../../theme";
+import styled from 'styled-components'
+import { colors, radius, spacing } from '../../theme'
 
 export const Uploader = styled.div`
   position: relative;
@@ -10,7 +10,7 @@ export const Uploader = styled.div`
   :focus-within {
     background-color: ${colors.gray[300]};
   }
-`;
+`
 
 export const Label = styled.label`
   display: block;
@@ -21,7 +21,7 @@ export const Label = styled.label`
   width: 100%;
   text-align: center;
   color: ${colors.gray[800]};
-`;
+`
 
 export const Input = styled.input`
   position: absolute;
@@ -31,4 +31,4 @@ export const Input = styled.input`
   cursor: pointer;
   width: 100%;
   height: 100%;
-`;
+`
